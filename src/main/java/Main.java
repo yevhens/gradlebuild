0992071de104dc341510d26b65feecb659b395bf
Yevhen <yevhensokyrko@gmail.com>
2018-07-21 14:45:04 +0300

    added jobarea enum and tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,11 @@ public class Main {
 
    public static void main(String[] args) {
 
+      Contact contact=new Contact("Andrey","Baldis",new org.joda.time.LocalDate(1956,6,17),445678999L,
+              675677878L,"hjhj@mail.ru",new JobArea[]{JobArea.AGRICULTURE,JobArea.PRODUCTION},Gender.MALE);
+
+      System.out.println(contact.calculateAge());
+
 
 
    }
